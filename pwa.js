c09780e1366e135146aae74624a02e5791d8a300
misinoe <misinoe.t@gmail.com>
2018-06-05 14:13:50 +0900

    update

--- pwa.js
+++ pwa.js
@@ -1,17 +1,19 @@
 !(() => {
   let defferEvent;
   window.addEventListener('beforeinstallprompt', e => {
-    console.log(e);
+    console.log('beforeinstallprompt', e);
     defferEvent = e;
     e.preventDefault();
     return false;
   });
 
   window.addEventListener('load', e => {
-    console.log(e);
+    console.log('load', e);
     const installButton = document.getElementById('installButton');
+    installButton.hidden = true;
     installButton.addEventListener('click', async e => {
-      console.log(e);
+      delete installButton.hidden;
+      console.log('click', e);
       await defferEvent.prompt();
       const result = await defferEvent.userChoise;
       console.log(result);
